08320095defc16a2d41b413936919b7b75729f40
ed <edburtnieks@pm.me>
2025-12-31 13:44:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Accessibility Cheatsheet
+
+https://a11ycheatsheet.com
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    const { children } = $props();
+</script>
+
+<svelte:head>
+    <title>Accessibility Cheatsheet</title>
+</svelte:head>
+
+{@render children()}
